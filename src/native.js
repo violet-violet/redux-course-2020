1059d5cb7c0ab541a6c274426056f3311ffe12e8
E. Demidovskaia <violet7693@yandex.ru>
2021-07-09 17:28:49 +0300

    vanilla js

--- src/native.js
+++ src/native.js
@@ -1,26 +1,34 @@
-import { createStore } from './createStore'
-import './styles.css'
-
 const counter = document.getElementById('counter'),
       addBtn = document.getElementById('add'),
       subBtn = document.getElementById('sub'),
       asyncBtn = document.getElementById('async'),
       themeBtn = document.getElementById('theme')
 
-const store = createStore()
+let state = 0
+
+function render() {
+    counter.textContent = state.toString()
+}
 
 addBtn.addEventListener('click', () => {
-
+    state++
+    render()
 })
 
 subBtn.addEventListener('click', () => {
-
+    state--
+    render()
 })
 
 asyncBtn.addEventListener('click', () => {
-
+    setTimeout(() => {
+        state++
+        render()
+    }, 2000)
 })
 
 themeBtn.addEventListener('click', () => {
-
+    document.body.classList.toggle('dark')
 })
+
+render()
